84080f43d561ba238dfd3c3a23a1bb64b6a1fdce
sheerinaseem <sheerin.naseem@gmail.com>
2016-04-09 03:55:33 -0400

    added new file

---
+++ file5.py
@@ -0,0 +1,17 @@
+my_name = "Sheerin Naseem"
+my_age = 22 #I'm sad about turning 23 :(
+my_height = 55 #inches
+my_weight = 46 #kgs
+my_eyes = "Brown" 
+my_teeth = "white"
+my_hair = "Black"
+
+print "Lets talk about %s." %my_name
+print "She is %d inches tall." %my_height
+print "She is %d kg heavy." %my_weight
+print "Actually that's very low on weighing scale."
+print "She has got %s eyes and %s hair." %(my_eyes, my_hair)
+print "Her teeth are usually %s depending upon the coffee." %my_teeth
+
+#this line is tricky, try to get it exactly right.
+print "If I add %d, %d and %d I get %d." %(my_age, my_height, my_weight, my_age + my_height + my_weight)
