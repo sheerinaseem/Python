2fbc7dcbb8d53ff401231140219fe690dea425fb
sheerinaseem <sheerin.naseem@gmail.com>
2016-04-10 16:40:59 -0400

    Redid the code

--- file3.py
+++ file3.py
@@ -12,6 +12,12 @@ print "Is it true that 3+2<5-7?" #This is a string
 print 3+2<5-7 #The print will be "False" since the calculation returns false
 
 print "What is 3+2?", 3+2
+print "What is 5 - 7?", 5 - 7
+print"Oh, that's why it's False!"
+
+print "How about some more?"
+print "Is it greater?", 5> -2
+print "Is it less or equal?", 5 <= -2
 
 print 7.0/4.0
 print 7/4
